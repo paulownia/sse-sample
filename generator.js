2c134e012aa63f4c2776524e4d2990c24cd9d116
nil paulownia <nil@paulownia.jp>
2011-01-10 17:46:45 +0900

    イベント発生間隔にゆらぎを作る

--- generator.js
+++ generator.js
@@ -9,13 +9,13 @@ emitter.data = "";
 emitter.start = function() {
 	var self = this;
 	setInterval(function() {
-		var pos = Math.floor(Math.random() * 7); 
+		var pos = Math.floor(Math.random() * chars.length * 2); 
 		if (pos < chars.length) {
 			self.id++;
 			self.data = chars.charAt(pos);
 			self.emit("generated", self.id, self.data);
 		}
-	}, 2000);
+	}, 1000);
 };
 
 module.exports = emitter; 
